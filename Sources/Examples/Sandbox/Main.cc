e7e26ccb4eedbe5011da60050d09f2b7f6eedbb3
futalihua esen <yokoshima.auharing@outlook.com>
2025-10-15 01:41:00 +0800

    adjust demo model file accordingly

--- Sources/Examples/Sandbox/Main.cc
+++ Sources/Examples/Sandbox/Main.cc
@@ -25,33 +25,33 @@ int main(int argc, char** argv){
     //always get dir using asset system
     fs::path modeldir = gameengine->getAssetSystem()->getModelDir();
     fs::path modelfile = modeldir / "fense" / "fense.obj";
-
-    //use path if possible to accesss new features
-    fleet::Model* woodfense = new fleet::Model{modelfile};
-    //bind shader to model
-    woodfense->setShader(phongShader);
-
-    fleet::Model* ironfense = new fleet::Model(modeldir / "fense2" / "fense.obj");
-    ironfense->setShader(phongShader);
-
+//
+//    //use path if possible to accesss new features
+//    fleet::Model* woodfense = new fleet::Model{modelfile};
+//    //bind shader to model
+//    woodfense->setShader(phongShader);
+//
+//    fleet::Model* ironfense = new fleet::Model(modeldir / "fense2" / "fense.obj");
+//    ironfense->setShader(phongShader);
+//
 //    fleet::Model* terrain = new fleet::Model(modeldir / "Rock_Terrain3_SF" / "Rock_Terrain3_SF.obj");
 //    terrain->setShader(phongShader);
 
-    fleet::Model* heli = new fleet::Model(modeldir / "mi35m" / "MI-35M.obj");
+    fleet::Model* heli = new fleet::Model(modeldir / "mi35m" / "mi35.glb");
     heli->setShader(phongShader);
 
     
 
     //add model to stage manager to display
-    EntityPtr woodfenseptr = std::make_shared<fleet::Entity>(woodfense);
-    gameengine->getStateManager()->addEntity(woodfenseptr);
-    auto model1transform = glm::translate(glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)), glm::vec3(0.0f, 0.0f, -5.0f));
-    woodfenseptr->setTransform(model1transform);
-
-    EntityPtr ironfenseptr = std::make_shared<fleet::Entity>(ironfense);
-    gameengine->getStateManager()->addEntity(ironfenseptr);
-    auto model2transform = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, -5.0f));
-    ironfenseptr->setTransform(model2transform);
+//    EntityPtr woodfenseptr = std::make_shared<fleet::Entity>(woodfense);
+//    gameengine->getStateManager()->addEntity(woodfenseptr);
+//    auto model1transform = glm::translate(glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)), glm::vec3(0.0f, 0.0f, -5.0f));
+//    woodfenseptr->setTransform(model1transform);
+//
+//    EntityPtr ironfenseptr = std::make_shared<fleet::Entity>(ironfense);
+//    gameengine->getStateManager()->addEntity(ironfenseptr);
+//    auto model2transform = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, -5.0f));
+//    ironfenseptr->setTransform(model2transform);
 
 //    EntityPtr terrainptr = std::make_shared<fleet::Entity>(terrain);
 //    gameengine->getStateManager()->addEntity(terrainptr);
